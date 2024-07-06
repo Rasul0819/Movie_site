04d603967f4c750585ea0f2a81643ebe4f157311
Rasul0819 <rasulabdikerimov85@gmail.com>
2024-07-06 13:02:24 +0500

    forms

---
+++ movies/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django import forms
+from . import models
+from django.contrib.auth.models import User
+class SignInForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ['username','password']
+
+class SingUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2',]
+        
+
+        
