00402b46f2e1082fee5b6806945814907355ff6f
Rasul0819 <rasulabdikerimov85@gmail.com>
2024-07-11 13:07:51 +0500

    signup and signin

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.movie_list,name='movie_list')
+    path('',views.movie_list,name='movie_list'),
+    path('login/',views.SingInClass.as_view(),name='login'),
+    path('signup/',views.registration,name='signup'),
+    path('logout/',views.log_out,name='logout'),
+    path('detail/<int:id>',views.post_detail,name='detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from . import models
-
-
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login,logout
+from . import forms
+from django.urls import reverse_lazy
 
 def movie_list(request,category_slug=None):
     category = None
@@ -14,4 +16,33 @@ def movie_list(request,category_slug=None):
         'categories':categories,
         'movies':movies
     })
-# Create your views here.
+
+
+class SingInClass(LoginView):
+    authentication_form = forms.SignInForm
+    template_name='sign_in.html'
+    # success_url=reverse_lazy('movie_list')
+    
+    # def get_success_url(self):
+    #     return reverse_lazy('movie_list')
+
+
+def registration(reqeust):
+    if reqeust.method =='POST':
+        form = forms.SingUpForm(reqeust.POST)
+        if form.is_valid():
+            user = form.save()
+            login(reqeust,user)
+            return redirect('movie_list')
+    else:
+        form = forms.SingUpForm()
+    return render(reqeust,'sign_up.html',{'form':form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect('movie_list')
+
+def post_detail(request,id):
+    movie = get_object_or_404(models.Movies,id=id)
+    return render(request,'movie_detail.html',{'movie':movie})
